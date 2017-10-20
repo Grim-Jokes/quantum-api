e6420433cc0a35ea75bccdad003a7fb27e003a45
Daniel Szekely <danszekely@gmail.com>
2017-10-19 21:28:47 -0400

    Add __str__ method to models

--- apps/transactions/models.py
+++ apps/transactions/models.py
@@ -18,9 +18,15 @@ class Category(models.Model):
         'transactions.Category', null=True, blank=True
     )
 
+    def __str__(self):
+        return f"{self.pk}. {self.name}"
+
 
 class Transaction(models.Model):
     date = models.DateField()
     description = models.CharField(max_length=100)
     value = models.DecimalField(decimal_places=2, max_digits=8)
     category = models.ForeignKey(Category, null=True)
+
+    def __str__(self):
+        return f"{self.description} - {self.value} - {self.date}"
